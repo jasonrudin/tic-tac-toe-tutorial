3891808dc0484c41f2b1198abd340a4b7c2eed0f
jasonrudin <j.b.rudin@gmail.com>
2022-02-18 08:08:55 -0500

    Refactored class updating to have two classes, winning square just modifies color now (vs. being a duplicate of square)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className= {props.winningSquare == true ? 'winningSquare' : 'square'}
+        <button className= {'square ' + (props.winningSquare == true ? 'winningSquare' : '')}
             onClick={props.onClick}
         >
             {props.value}
